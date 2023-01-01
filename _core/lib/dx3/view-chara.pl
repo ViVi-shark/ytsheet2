893227b0eef31585a346f14a1bf95f7e084fd5ef
ViVi <vivi.shark.tech+github@gmail.com>
2023-01-01 17:54:27 +0900

    [DX3] セッション履歴がなにも記入されていなければセクションごと非表示にする

--- _core/lib/dx3/view-chara.pl
+++ _core/lib/dx3/view-chara.pl
@@ -709,6 +709,7 @@ foreach (0 .. $pc{historyNum}){
   } );
 }
 $SHEET->param(History => \@history);
+$SHEET->param(HistoryRowCount => $#history);
 
 ### バックアップ --------------------------------------------------
 if($::in{id}){
