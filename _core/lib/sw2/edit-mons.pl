51c9e27b9686ba8c469e9f0032f80417706ef3a6
ViVi <vivi.shark.tech+github@gmail.com>
2024-03-30 15:15:57 +0900

    wip : [SW2] 魔物データの個別化機能

--- _core/lib/sw2/edit-mons.pl
+++ _core/lib/sw2/edit-mons.pl
@@ -693,13 +693,13 @@ print <<"HTML";
       </p>
     </form>
 HTML
-    print "<fieldset id=\"loaded-data\" style=\"display: none;\">\n";
-    for my $key (keys %pc) {
-        next if $key !~ /^partEquipment\d|^golemReinforcement_(?:[A-Za-z]+_part(?:\d+|All)_using|quartzDisruption_attribute)$|^swordFragment_[hm]pOffset_part\d+$/;
-        print "<input type=\"hidden\" name=\"$key\" value=\"$pc{$key}\" />\n";
-    }
-    print "</fieldset>\n";
 }
+print "<fieldset id=\"loaded-data\" style=\"display: none;\">\n";
+for my $key (keys %pc) {
+    next if $key !~ /^partEquipment\d|^golemReinforcement_(?:[A-Za-z]+_part(?:\d+|All)_using|quartzDisruption_attribute)$|^swordFragment_[hm]pOffset_part\d+$/;
+    print "<input type=\"hidden\" name=\"$key\" value=\"$pc{$key}\" />\n";
+}
+print "</fieldset>\n";
 print <<"HTML";
     </article>
 HTML
