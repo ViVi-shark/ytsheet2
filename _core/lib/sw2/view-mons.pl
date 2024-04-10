542ef9009fb8cc8f1bc5485b58c5a0deab73884c
ViVi <vivi.shark.tech+github@gmail.com>
2024-04-10 14:55:18 +0900

    wip : [SW2] ゴーレムデータの個別化機能（戦利品の無効化）

--- _core/lib/sw2/view-mons.pl
+++ _core/lib/sw2/view-mons.pl
@@ -561,6 +561,7 @@ foreach (1 .. $pc{lootsNum}){
     ITEM => $pc{'loots'.$_.'Item'},
   } );
 }
+@loots = () if $pc{individualization} && $pc{disableLoots};
 $SHEET->param(Loots => \@loots);
 
 ### バックアップ --------------------------------------------------
