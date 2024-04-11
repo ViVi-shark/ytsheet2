f19f6916ee3f07c81ecd27e3c07fd65179311f0b
ViVi <vivi.shark.tech+github@gmail.com>
2024-04-11 18:01:17 +0900

    アイテムの種別に「カスタマイズ」関連を追加
    # Conflicts:
    #	_core/lib/dx3/edit-chara.pl

--- _core/lib/dx3/edit-chara.pl
+++ _core/lib/dx3/edit-chara.pl
@@ -1340,6 +1340,7 @@ print <<"HTML";
     <option value="エンブレム／射撃">
     <option value="リレーション／白兵">
     <option value="リレーション／射撃">
+    <option value="カスタマイズ（武器）">
   </datalist>
   <datalist id="list-armor-type">
     <option value="防具">
@@ -1348,10 +1349,12 @@ print <<"HTML";
     <option value="エンブレム／防具">
     <option value="エンブレム／防具（補助）">
     <option value="リレーション／防具">
+    <option value="カスタマイズ（防具）">
   </datalist>
   <datalist id="list-vehicle-type">
     <option value="ヴィークル">
     <option value="エンブレム／ヴィークル">
+    <option value="カスタマイズ（ヴィークル）">
   </datalist>
   <datalist id="list-item-type">
     <option value="コネ">
@@ -1366,6 +1369,8 @@ print <<"HTML";
     <option value="リレーション／一般">
     <option value="リレーション／その他">
     <option value="リレーション／使い捨て">
+    <option value="カスタマイズ（一般）">
+    <option value="カスタマイズ（コネ）">
   </datalist>
   <datalist id="list-dfclty">
     <option value="―">
