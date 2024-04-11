1aa0720de8530ac106fa657d9cc8349867e87781
ViVi <vivi.shark.tech+github@gmail.com>
2024-04-11 11:14:47 +0900

    [DX3] コンボが１件もなければ、コンボ欄そのものを非表示にする
    # Conflicts:
    #	_core/lib/dx3/view-chara.pl

--- _core/lib/dx3/view-chara.pl
+++ _core/lib/dx3/view-chara.pl
@@ -518,6 +518,7 @@ $SHEET->param(Magics => \@magics);
 
 ### コンボ --------------------------------------------------
 my @combos;
+my $comboNum = 0;
 foreach (1 .. $pc{comboNum}){
   next if !existsRow "combo$_",'Name','Combo','Timing','Skill','Dfclty','Target','Range','Encroach','Note','Dice1','Crit1','Atk1','Fixed1';
   my $blankrow = 0;
@@ -589,8 +590,10 @@ foreach (1 .. $pc{comboNum}){
     "EXCLUDE_DICE_ROLL" => $excludeDiceRoll ? 'yes' : 'no',
     "EXCLUDE_ATK" => $excludeAttack ? 'yes' : 'no',
   });
+  $comboNum++;
 }
 $SHEET->param(Combos => \@combos);
+$SHEET->param(ComboNum => $comboNum);
 sub textCombo {
   my $text = shift;
   if($text =~ /《.*?》/){
