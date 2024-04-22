a2bf6936b9264cde585df2740796b0c89d8d66d7
ViVi <vivi.shark.tech+github@gmail.com>
2024-04-22 11:07:45 +0900

    明示的な改行ごとに p 要素を分割する
    # Conflicts:
    #	_core/lib/dx3/view-chara.pl
    #	_core/lib/sw2/view-chara.pl

--- _core/lib/subroutine.pl
+++ _core/lib/subroutine.pl
@@ -705,6 +705,17 @@ sub removeRuby {
   return $text;
 }
 
+sub splitParagraph {
+  my $text = shift;
+
+  $text =~ s#<br>#</p><p>#gi;
+  $text =~ s#(?:<p></p>)+<p>#<p class="before-margin">#gi;
+  $text =~ s#<p></p><(h[1-6])>#<$1>#gi;
+  $text =~ s#(^</p><p>|</p><p>$)##gi;
+
+  return $text;
+}
+
 ### RGB>HSL --------------------------------------------------
 sub rgb_to_hsl {
   my $re = shift || 0;
--- _core/lib/sw2/view-chara.pl
+++ _core/lib/sw2/view-chara.pl
@@ -123,6 +123,7 @@ if($pc{ver}){
       $pc{$_} = unescapeTagsLines($pc{$_});
     }
     $pc{$_} = unescapeTags($pc{$_});
+    $pc{$_} = splitParagraph($pc{$_}) if $_ =~ /^(?:items|freeNote|freeHistory|cashbook)$/;
 
     $pc{$_} = noiseTextTag $pc{$_} if $pc{forbiddenMode};
   }
