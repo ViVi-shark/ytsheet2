9f9e81f5ab272c8a0e027e30f2cba103d7755277
ViVi <vivi.shark.tech+github@gmail.com>
2024-05-14 17:41:48 +0900

    [SW2] Refactor: split notice to paragraphs by line

--- _core/lib/sw2/edit-chara.js
+++ _core/lib/sw2/edit-chara.js
@@ -2233,7 +2233,17 @@ function checkLanguage(){
       }
     }
   }
-  document.getElementById('language-notice').innerHTML = notice;
+  const noticeContainer = document.getElementById('language-notice');
+  noticeContainer.innerHTML = '';
+  noticeContainer.append(
+      ...(notice.split('<br>').filter(x => x !== '').map(
+          notice => {
+            const p = document.createElement('p');
+            p.textContent = notice;
+            return p;
+          }
+      ))
+  );
 }
 // 追加
 function addLanguage(){
