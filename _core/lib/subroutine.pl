079c341fc5b7d927da1c39bbfdd9acff4daf7626
ViVi <vivi.shark.tech+github@gmail.com>
2024-06-02 21:43:07 +0900

    自動的に字下げする [fix up]
    手動の字下げを除去する

--- _core/lib/subroutine.pl
+++ _core/lib/subroutine.pl
@@ -712,6 +712,7 @@ sub splitParagraph {
   $text =~ s#<br>#</p><p>#gi;
   $text =~ s#(?:<p></p>)+<p>#<p class="before-margin">#gi;
   $text =~ s#<p></p><(h[1-6])>#<$1>#gi;
+  $text =~ s#(<p>)　#$1#gi;
   $text =~ s#(^</p><p>|</p><p>$)##gi;
 
   return $text;
