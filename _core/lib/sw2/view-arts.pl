5bc27f58c4eaf89159ae295ee0d217a8406390b0
ViVi <vivi.shark.tech+github@gmail.com>
2024-10-02 19:38:03 +0900

    自動的に字下げする [fix up]
    魔法の効果

--- _core/lib/sw2/view-arts.pl
+++ _core/lib/sw2/view-arts.pl
@@ -251,6 +251,8 @@ $SHEET->param(Tags => \@tags);
   else {
     magicItemViewOn('Cost','Target','Range','Duration','Resist',($pc{magicElement}?'Element':undef));
   }
+
+  $SHEET->param(magicEffect => splitParagraph $pc{magicEffect});
 }
 sub textMagic {
   $_[0] =~ s#／#／<wbr>#;
