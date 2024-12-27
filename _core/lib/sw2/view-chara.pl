a147f4311e63acaaa10fd224321724e44ededd50
ViVi <vivi.shark.tech+github@gmail.com>
2024-12-27 10:30:42 +0900

    [SW2.5] 閲覧画面において、個別の不名誉点の値をカンマ区切りにする

--- _core/lib/sw2/view-chara.pl
+++ _core/lib/sw2/view-chara.pl
@@ -1088,7 +1088,7 @@ foreach (1 .. $pc{dishonorItemsNum}) {
   elsif($pc{"dishonorItem${_}PtType"} eq 'dragon'  ){ $type = '<small>竜</small>'; }
   push(@dishonoritems, {
     NAME => $pc{'dishonorItem'.$_},
-    PT   => $type.$pc{'dishonorItem'.$_.'Pt'},
+    PT   => commify($type.$pc{'dishonorItem'.$_.'Pt'}),
   } );
 }
 $SHEET->param(DishonorItems => \@dishonoritems);
