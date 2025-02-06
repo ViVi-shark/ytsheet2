f937aa5dbf52a803b315737a0f88be63c6ec2f74
ViVi <vivi.shark.tech+github@gmail.com>
2025-02-06 13:03:17 +0900

    Fix: [SW2] 流派データに流派装備を追加した時点では、当該アイテムが複数のカテゴリをもつ可能性が考慮されていない不具合を修正

--- _core/lib/sw2/edit-arts.js
+++ _core/lib/sw2/edit-arts.js
@@ -208,7 +208,7 @@ async function addSchoolItem(){
       }
       tr.innerHTML = `
         <td><a href="${url}">${itemName}</a></td>
-        <td>${data.category||''}</td>
+        <td>${data?.category.replace(/\s+/, '<hr>') ?? ''}</td>
         <td>${data.summary ||''}</td>
         <td class="button" onclick="delSchoolItem(this,'${url}')">×</td>
       `;
