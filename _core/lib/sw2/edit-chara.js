b7652c743d1bbb6fb21e1489c69f8d034f0c66b8
ViVi <vivi.shark.tech+github@gmail.com>
2025-03-03 11:31:53 +0900

    Fix: [SW2] 編集画面で《魔力強化》による増分が正しく表示されない不具合を修正

--- _core/lib/sw2/edit-chara.js
+++ _core/lib/sw2/edit-chara.js
@@ -1295,6 +1295,8 @@ function calcMagic() {
   document.getElementById("magic-power-common"      ).style.display = openMagic              ? '' : 'none';
   document.getElementById("magic-power-hr"          ).style.display = openMagic && openCraft ? '' : 'none';
 
+  document.getElementById('magic-power-magicenhance-value').textContent = feats['魔力強化']?.toString();
+
   stylizeVisibleRows(document.querySelectorAll('#magic-power > .edit-table > tbody > tr'))
 }
 
