31fb9a2990c9330290b2b11fce04b2c3b38d3aed
ViVi <vivi.shark.tech+github@gmail.com>
2025-07-09 11:54:52 +0900

    Fix: [SW2] 技芸データの「対象」「射程」の入力候補では、「術者」ではなく「自身」とする

--- _core/lib/sw2/edit-arts.pl
+++ _core/lib/sw2/edit-arts.pl
@@ -580,7 +580,7 @@ print <<"HTML";
     <option value="2d(9)HP">
   </datalist>
   <datalist id="list-target">
-    <option value="術者">
+    <option value="術者" class="self">
     <option value="1体">
     <option value="1体全">
     <option value="1体X">
@@ -605,7 +605,7 @@ print <<"HTML";
     <option value="全エリア(半径30m)／空間">
   </datalist>
   <datalist id="list-range">
-    <option value="術者">
+    <option value="術者" class="self">
     <option value="接触">
     <option value="1(10m)">
     <option value="2(20m)">
