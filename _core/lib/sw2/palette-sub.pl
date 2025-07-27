18c8338b21368306817251b60ea6805bbbff0c34
ViVi <vivi.shark.tech+github@gmail.com>
2025-07-27 17:57:26 +0900

    Fix: [SW2] 騎獣用防具による防護点の上昇がチャットパレットに反映されていなかったのを修正

--- _core/lib/sw2/palette-sub.pl
+++ _core/lib/sw2/palette-sub.pl
@@ -1461,6 +1461,7 @@ sub palettePreset {
           $::pc{'status' . $num . 'Accuracy'} += $::pc{'partEquipment' . $_ . '-weapon-accuracy'} if $::pc{'status' . $num . 'Accuracy'} ne '';
           $::pc{'status' . $num . 'Damage'} = addOffsetToDamage($::pc{'status' . $num . 'Damage'}, $::pc{'partEquipment' . $_ . '-weapon-damage'}) if $::pc{'status' . $num . 'Damage'} ne '';
           $::pc{'status' . $num . 'Evasion'} += $::pc{'partEquipment' . $_ . '-armor-evasion'} if $::pc{'status' . $num . 'Evasion'} ne '';
+          $::pc{'status' . $num . 'Defense'} += $::pc{'partEquipment' . $_ . '-armor-defense'} if $::pc{'status' . $num . 'Defense'} ne '';
 
           my $partName = $::pc{'status' . $_ . 'Style'};
           if ($partName) {
